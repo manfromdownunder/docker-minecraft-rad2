0f1c9b28b1ae6c6c842c04a43506324b4b5aaecd
root <manfromdownunder@hotmail.com>
2023-10-07 14:55:01 +1300

    fixed startup script

--- start-server.sh
+++ start-server.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-# Validate environment variables, if needed
+# Define the path to the flag file
+FLAG_FILE="/minecraft-server/first_run_completed.flag"
 
-# Start the Minecraft server
-java -Xmx${JAVA_MEMORY_MAX} -Xms${JAVA_MEMORY_MIN} -XX:PermSize=${JAVA_PERM_SIZE} -jar forge-${MINECRAFT_VERSION}-${FORGE_VERSION}.jar nogui
+# Validate environment variables, if needed
+# (your validation code here, if any)
+
+# Check for the flag file
+if [ ! -f "$FLAG_FILE" ]; then
+  # Flag file doesn't exist, this must be the first run
+  # Create the flag file
+  touch "$FLAG_FILE"
+  echo "First run detected, flag file created. Exiting."
+  exit 0
+else
+  # Flag file exists, proceed to start the Minecraft server
+  echo "Flag file detected, starting Minecraft server."
+  java -Xmx${JAVA_MEMORY_MAX} -Xms${JAVA_MEMORY_MIN} -XX:PermSize=${JAVA_PERM_SIZE} -jar forge-${MINECRAFT_VERSION}-${FORGE_VERSION}.jar nogui
+fi
