e562d98b1dd14c779110ea031e4f1f04ac5061c8
root <manfromdownunder@hotmail.com>
2023-10-24 19:27:03 +1300

    feat: fixed startup script

--- start-server.sh
+++ start-server.sh
@@ -2,7 +2,7 @@
 
 start_server(){
 echo "Starting Minecraft server."
-screen -S minecraft -d -m java -Xmx${JAVA_MEMORY_MAX} -Xms${JAVA_MEMORY_MIN} -XX:PermSize=${JAVA_PERM_SIZE} -jar /minecraft/server/forge-${MINECRAFT_VERSION}-${FORGE_VERSION}.jar nogui
+java -Xmx${JAVA_MEMORY_MAX} -Xms${JAVA_MEMORY_MIN} -XX:PermSize=${JAVA_PERM_SIZE} -jar /minecraft/server/forge-${MINECRAFT_VERSION}-${FORGE_VERSION}.jar nogui
 }
 
 echo "Starting server"
