6c6df20ead1286b66bb3b9693b27cb45c84411c0
root <manfromdownunder@hotmail.com>
2023-10-24 19:26:30 +1300

    feat: fixed startup script

--- start-server.sh
+++ start-server.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+start_server(){
 echo "Starting Minecraft server."
 screen -S minecraft -d -m java -Xmx${JAVA_MEMORY_MAX} -Xms${JAVA_MEMORY_MIN} -XX:PermSize=${JAVA_PERM_SIZE} -jar /minecraft/server/forge-${MINECRAFT_VERSION}-${FORGE_VERSION}.jar nogui
+}
 
+echo "Starting server"
+rm -f autostart.stamp
+start_server
+
+while [ -e autostart.stamp ] ; do
+    rm -f autostart.stamp
+    echo "If you want to completely stop the server process now, press Ctrl+C before the time is up!"
+    for i in 5 4 3 2 1; do
+        echo "Restarting server in $i"
+        sleep 1
+    done
+    echo "Rebooting now!"
+    start_server
+    echo "Server process finished"
+done
